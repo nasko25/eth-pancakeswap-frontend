b8057057fe527cc372a0a2a6250c093235c16649
nasko25 <apashov0@gmail.com>
2021-08-02 21:53:04 +0300

    feat: Added eth chain ids to the NETWORK_URLS in networks.ts

--- src/config/constants/networks.ts
+++ src/config/constants/networks.ts
@@ -1,11 +1,17 @@
 import { ChainId } from '@pancakeswap/sdk'
+import { ChainId as ethChainId } from '@uniswap/sdk'
 
 // TODO update ChainId everywhere
 //import { ChainId } from '@uniswap/sdk'
 
-const NETWORK_URLS: { [chainId in ChainId]: string } = {
+const NETWORK_URLS: { [chainId in ChainId & ethChainId]: string } = {
   [ChainId.MAINNET]: 'https://bsc-dataseed1.defibit.io',
   [ChainId.TESTNET]: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+  [ethChainId.MAINNET]: 'https://main-light.eth.linkpool.io',
+  [ethChainId.ROPSTEN]: 'https://ropsten.infura.io',
+  [ethChainId.RINKEBY]: 'https://rinkeby-light.eth.linkpool.io',
+  [ethChainId.GÖRLI]: 'https://goerli-light.eth.linkpool.io',
+  [ethChainId.KOVAN]: 'https://kovan.infura.io',
 }
 
 // TODO probably use infura with: `https://rinkeby.infura.io/v3/${process.env.PROJECT_ID}`
