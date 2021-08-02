7a7eed0151d4f109843e2c46e1b40a438eeac5fb
nasko25 <apashov0@gmail.com>
2021-08-02 23:58:32 +0300

    chore: Added some contract addresses and comments in contracts.ts
    Some contracts will probably have to be deployed to ropsten.

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,9 +1,12 @@
 export default {
   masterChef: {
-    97: '0x1d32c2945C8FDCBc7156c553B7cEa4325a17f4f9',
-    56: '0x73feaa1eE314F8c655E354234017bE2193C9E24E',
+    // https://github.com/pancakeswap/pancake-farm/blob/master/contracts/MasterChef.sol
+    97: '0x1d32c2945C8FDCBc7156c553B7cEa4325a17f4f9', // bsc testnet
+    56: '0x73feaa1eE314F8c655E354234017bE2193C9E24E', // bsc mainnet
+    3: 'TODO', // ropsten  // TODO deploy
   },
   sousChef: {
+    // https://github.com/pancakeswap/pancake-farm/blob/master/contracts/SousChef.sol
     97: '0xd3af5fe61dbaf8f73149bfcfa9fb653ff096029a',
     56: '0x6ab8463a4185b80905e05a9ff80a2d6b714b9e95',
   },
@@ -12,6 +15,7 @@ export default {
     56: '0x5aF6D33DE2ccEC94efb1bDF8f92Bd58085432d2c',
   },
   multiCall: {
+    // https://github.com/pancakeswap/pancake-farm/blob/master/contracts/libs/Multicall.sol
     56: '0xfF6FD90A470Aaa0c1B8A54681746b07AcdFedc9B',
     97: '0x8F3273Fb89B075b1645095ABaC6ed17B2d4Bc576',
   },
@@ -56,8 +60,11 @@ export default {
     97: '0x4f3140C74789F1D809420343ea83BcE52B7bbAA5',
   },
   chainlinkOracle: {
+    // EACAggregatorProxy
     56: '0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE',
     97: '',
+    1: '0x7c5d4F8345e66f68099581Db340cd65B078C41f4', // eth mainnet
+    3: '0x1e5a19e9f382DD254D6983FF253A64F0bF15058a',
   },
   bunnySpecialCakeVault: {
     56: '0x5B4a770Abe7Eafb2601CA4dF9d73EA99363E60a4',
