4b9ef3d5fb9d825e9c86345c1f01f0488ce5b833
nasko25 <apashov0@gmail.com>
2021-08-02 22:05:56 +0300

    feat: Added MULTICALL_NETWORKS addresses for eth mainnet and ropsten

--- src/config/constants/multicall/index.ts
+++ src/config/constants/multicall/index.ts
@@ -1,9 +1,12 @@
 import { ChainId } from '@pancakeswap/sdk'
+import { ChainId as ethChainId } from '@uniswap/sdk'
 import MULTICALL_ABI from './abi.json'
 
-const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
+const MULTICALL_NETWORKS: { [chainId in ChainId & ethChainId]: string } = {
   [ChainId.MAINNET]: '0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb',
   [ChainId.TESTNET]: '0x301907b5835a2d723Fe3e9E8C5Bc5375d5c1236A',
+  [ethChainId.MAINNET]: '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441',
+  [ethChainId.ROPSTEN]: '0x53C43764255c17BD724F74c4eF150724AC50a3ed',
 }
 
 export { MULTICALL_ABI, MULTICALL_NETWORKS }
