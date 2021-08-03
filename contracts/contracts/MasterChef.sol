18bb2d254e52486ccb2c50eac27f9d59cf9b6af1
nasko25 <apashov0@gmail.com>
2021-08-03 16:10:03 +0300

    chore: Removed @pancakeswap/pancake-swap-lib from the contracts packages
    and added back the 'Info' button in the menu

--- contracts/contracts/MasterChef.sol
+++ contracts/contracts/MasterChef.sol
@@ -1,8 +1,6 @@
 pragma solidity 0.8.6;
 
 // TODO change occurences of Cake to RBRY
-// TODO everything should be available from openzeppelin
-//  remove @pancakeswap/pancake-swap-lib from package.json when it is no longer needed
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
