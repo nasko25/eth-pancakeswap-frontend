05eee0fd4d07c22a8a27df4cfae3c2ce658b1c3d
nasko25 <apashov0@gmail.com>
2021-08-04 23:52:02 +0300

    docs: Added comments for the contracts in contracts.ts

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -32,38 +32,60 @@ export default {
     97: '0x4B683C7E13B6d5D7fd1FeA9530F451954c1A7c8A',
   },
   pancakeRabbits: {
+    // PancakeBunnies ERC721 contract
+    // probably connected to the pancake profile
+    // https://bscscan.com/address/0xDf7952B35f24aCF7fC0487D01c8d5690a60DBa07#code
     56: '0xDf7952B35f24aCF7fC0487D01c8d5690a60DBa07',
     97: '0x60935F36e4631F73f0f407e68642144e07aC7f5E',
   },
   bunnyFactory: {
+    // BunnyFactoryV3
+    // probably connected to the pancake profile
+    // https://bscscan.com/address/0xfa249Caa1D16f75fa159F7DFBAc0cC5EaB48CeFf#code
     56: '0xfa249Caa1D16f75fa159F7DFBAc0cC5EaB48CeFf',
     97: '0x707CBF373175fdB601D34eeBF2Cf665d08f01148',
   },
   claimRefund: {
+    // ClaimBackCake
+    // https://bscscan.com/address/0xE7e53A7e9E3Cf6b840f167eF69519175c497e149#code
     56: '0xE7e53A7e9E3Cf6b840f167eF69519175c497e149',
     97: '',
   },
   pointCenterIfo: {
+    // XXX: It will probably be useful
+    // "contract for users to collect points for IFOs they participated in"
+    // https://bscscan.com/address/0x3C6919b132462C1FEc572c6300E83191f4F0012a#code
     56: '0x3C6919b132462C1FEc572c6300E83191f4F0012a',
     97: '0xd2Ac1B1728Bb1C11ae02AB6e75B76Ae41A2997e3',
   },
   bunnySpecial: {
+    // BunnySpecialV2
+    // https://bscscan.com/address/0xFee8A195570a18461146F401d6033f5ab3380849#code
     56: '0xFee8A195570a18461146F401d6033f5ab3380849',
     97: '0x7b7b1583De1DeB32Ce6605F6deEbF24A0671c17C',
   },
   tradingCompetition: {
+    // XXX: It is probably important
+    // "contract for users to collect points based on off-chain events"
     56: '0xd718baa0B1F4f70dcC8458154042120FFE0DEFFA',
     97: '0xC787F45B833721ED3aC46E99b703B3E1E01abb97',
   },
   easterNft: {
+    // BunnySpecialV2
+    // "contract for users to mint exclusive Easter collectibles for their teams"
     56: '0x23c41D28A239dDCAABd1bb1deF8d057189510066',
     97: '0x24ec6962dbe874F6B67B5C50857565667fA0854F',
   },
   cakeVault: {
+    // TODO deploy?
+    // "Auto CAKE Syrup Pool Smart Contract"
+    // https://bscscan.com/address/0xa80240Eb5d7E05d3F250cF000eEc0891d00b51CC#code
     56: '0xa80240Eb5d7E05d3F250cF000eEc0891d00b51CC',
     97: '',
   },
   predictions: {
+    // BnbPricePrediction
+    // https://bscscan.com/address/0x516ffd7D1e0Ca40b1879935B2De87cb20Fc1124b#code
     56: '0x516ffd7D1e0Ca40b1879935B2De87cb20Fc1124b',
     97: '0x4f3140C74789F1D809420343ea83BcE52B7bbAA5',
   },
@@ -75,14 +97,19 @@ export default {
     3: '0x1e5a19e9f382DD254D6983FF253A64F0bF15058a', // ropsten eth testnet
   },
   bunnySpecialCakeVault: {
+    // BunnySpecialCakeVault
+    // "contract for users to mint Cake Vault collectible"
     56: '0x5B4a770Abe7Eafb2601CA4dF9d73EA99363E60a4',
     97: '',
   },
   bunnySpecialPrediction: {
+    // contract for users to mint exclusive collectibles if they participated in Prediction beta.
     56: '0x342c99e9aC24157657095eC69CB04b73257e7A9C',
     97: '',
   },
   farmAuction: {
+    // FarmAuction
+    // https://bscscan.com/address/0xb92Ab7c1edcb273AbA24b0656cEb3681654805D2#code
     56: '0xb92Ab7c1edcb273AbA24b0656cEb3681654805D2',
     97: '0x3F9602593b4f7C67ab045DB51BbDEa94E40fA9Fe',
   },
