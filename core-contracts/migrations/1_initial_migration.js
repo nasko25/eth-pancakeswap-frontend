efaf2f0e195d5a4dbd39f1c47ebd56b622b124e1
nasko25 <apashov0@gmail.com>
2021-08-23 15:15:47 +0300

    fix: Added RaspberryERC20's address to RaspberryProfile's constructor call

--- core-contracts/migrations/1_initial_migration.js
+++ core-contracts/migrations/1_initial_migration.js
@@ -4,11 +4,13 @@ const RaspberryFactory = artifacts.require("RaspberryFactory");
 const RaspberryProfile = artifacts.require("RaspberryProfile");
 
 module.exports = function (deployer) {
-  deployer.deploy(RaspberryToken);
+  deployer.deploy(RaspberryToken).then(() => {
+    // https://bscscan.com/address/0xDf4dBf6536201370F95e06A0F8a7a70fE40E388a#code
+    deployer.deploy(RaspberryProfile, RaspberryToken.address, "0x0000000000000000000000000000000000000000000000001bc16d674ec80000", "0x0000000000000000000000000000000000000000000000000de0b6b3a7640000", "0x0000000000000000000000000000000000000000000000001bc16d674ec80000");
+  });
+
   deployer.deploy(RaspberryPair);
 
   // https://bscscan.com/address/0xbcfccbde45ce874adcb698cc183debcf17952812#code
   deployer.deploy(RaspberryFactory, "0x0000000000000000000000000f9399fc81dac77908a2dde54bb87ee2d17a3373");
-  // https://bscscan.com/address/0xDf4dBf6536201370F95e06A0F8a7a70fE40E388a#code
-  deployer.deploy(RaspberryProfile, "TODO raspberry token address", "0x0000000000000000000000000000000000000000000000001bc16d674ec80000", "0x0000000000000000000000000000000000000000000000000de0b6b3a7640000", "0x0000000000000000000000000000000000000000000000001bc16d674ec80000");
 };
