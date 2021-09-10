6369630245ce33272b7f326b4af979b63175d8e0
nasko25 <apashov0@gmail.com>
2021-09-10 11:56:27 +0300

    fix: Adding addresses to 2_deploy_contracts.js

--- contracts/migrations/2_deploy_contracts.js
+++ contracts/migrations/2_deploy_contracts.js
@@ -4,13 +4,16 @@ const RBRYToken = artfacts.require("RBRYToken");
 const SousChef = artifacts.require("SousChef");
 const SyrupBar = artifacts.require("SyrupBar");
 module.exports = function(deployer) {
-																		                                                                  // RBRY per block; value taken from the bsc masterchef deployed contract    // start block
-    deployer.deploy(MasterChef, "TODO: raspberry token contract address", "TODO: syrupbar token contract address", "TODO: contract deployer address", "0x0000000000000000000000000000000000000000000000022b1c8c1227a00000", 10600005);
     // deployer.deploy(Multicall);
-    deployer.deploy(RBRYToken);
-                                                            // reward per block; value taken from the bsc souschef deployed contract    // start block  // end block
-    deployer.deploy(SousChef, "TODO: syrupbar token contract address", "0x0000000000000000000000000000000000000000000000001bc16d674ec80000", 10900005, (10900005 + 150000));
-    deployer.deploy(SyrupBar, "TODO: raspberry token contract address");
+    deployer.deploy(RBRYToken).then(() => {
+      deployer.deploy(SyrupBar, RBRYToken.address).then(() => {
+                                          // reward per block; value taken from the bsc souschef deployed contract    // start block  // end block
+        deployer.deploy(SousChef, SyrupBar.address, "0x0000000000000000000000000000000000000000000000001bc16d674ec80000", 10900005, (10900005 + 150000));
+                                                                                                  // RBRY per block; value taken from the bsc masterchef deployed contract    // start block
+        deployer.deploy(MasterChef, RBRYToken.address, SyrupBar.address, "TODO: contract deployer address", "0x0000000000000000000000000000000000000000000000022b1c8c1227a00000", 10600005);
+      });
+
+    });
     // Additional contracts can be deployed here
 };
 
