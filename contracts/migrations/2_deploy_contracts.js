1e71b5df9fc2f7504fc9df9d2a6fc92a30716176
nasko25 <apashov0@gmail.com>
2021-09-22 01:35:57 +0300

    fix: Copied the core-contracts/'s truffle config to contracts/ and fixed a bug in 2_deploy_contracts

--- contracts/migrations/2_deploy_contracts.js
+++ contracts/migrations/2_deploy_contracts.js
@@ -1,16 +1,16 @@
 const MasterChef = artifacts.require("MasterChef");
 // const Multicall = artifacts.require("Multicall");
-const RBRYToken = artfacts.require("RBRYToken");
+const RBRYToken = artifacts.require("RBRYToken");
 const SousChef = artifacts.require("SousChef");
 const SyrupBar = artifacts.require("SyrupBar");
 module.exports = function(deployer) {
     // deployer.deploy(Multicall);
-    deployer.deploy(RBRYToken).then(() => {
-      deployer.deploy(SyrupBar, RBRYToken.address).then(() => {
-                                          // reward per block; value taken from the bsc souschef deployed contract    // start block  // end block
-        deployer.deploy(SousChef, SyrupBar.address, "0x0000000000000000000000000000000000000000000000001bc16d674ec80000", 10900005, (10900005 + 150000));
-                                                                                                  // RBRY per block; value taken from the bsc masterchef deployed contract    // start block
-        deployer.deploy(MasterChef, RBRYToken.address, SyrupBar.address, "TODO: contract deployer address", "0x0000000000000000000000000000000000000000000000022b1c8c1227a00000", 10600005);
+    deployer.deploy(RBRYToken).then(async () => {
+      await deployer.deploy(SyrupBar, RBRYToken.address).then(async () => {
+                                               // reward per block; value taken from the bsc souschef deployed contract    // start block  // end block
+        await deployer.deploy(SousChef, SyrupBar.address, "0x0000000000000000000000000000000000000000000000001bc16d674ec80000", 10900005, (10900005 + 150000));
+                                                                                                                   // RBRY per block; value taken from the bsc masterchef deployed contract    // start block
+        await deployer.deploy(MasterChef, RBRYToken.address, SyrupBar.address, "0x145abf73a136dc3ad9baa9f123553dfcfd716d74", "0x0000000000000000000000000000000000000000000000022b1c8c1227a00000", 10600005);
       });
 
     });
