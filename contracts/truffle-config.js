7289ec26273ffb6fc9d39e5d6ca172ccb5b7bf29
nasko25 <apashov0@gmail.com>
2022-01-09 23:00:18 +0200

    feat: Started adding LinkToken to the contracts

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -83,7 +83,7 @@ module.exports = {
   compilers: {
     solc: {
       // version: "0.5.16",    // Fetch exact version from solc-bin (default: truffle's version)
-      version: "0.8.11",
+      version: "0.8.6",
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
