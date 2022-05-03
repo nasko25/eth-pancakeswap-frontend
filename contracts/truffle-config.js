87bc6b24fecdfb6bfe0e180b54b6ffaeb0a4b08c
nasko25 <apashov0@gmail.com>
2022-05-03 19:38:50 +0200

    build: Copied pancakeswap cake vault

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -25,7 +25,7 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { projectId, mnemonic, etherscan_key } = require("../core-contracts/secrets.json");
 
 module.exports = {
-  contracts_directory: "./contracts/lottery",
+  contracts_directory: "./contracts/vault",
   /**
    * Networks define how you connect to your ethereum client and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
@@ -85,7 +85,7 @@ module.exports = {
   compilers: {
     solc: {
       // version: "0.5.16",    // Fetch exact version from solc-bin (default: truffle's version)
-      version: "0.8.6",
+      version: "0.6.12",
       // version: "0.6.6",
       // version: "0.4.16",
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
